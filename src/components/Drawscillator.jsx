c921cc2fbbf84e383caaca4b957e3bd9d3b80d3e
kimestelle <137699667+kimestelle@users.noreply.github.com>
2025-05-22 19:00:37 -0700

    new routing yay

---
+++ src/components/Drawscillator.jsx
@@ -0,0 +1,8 @@
+export function Drawscillator() {
+    return (
+        <div className="drawscillator">
+            <h1>Welcome to the Drawscillator Page</h1>
+            <p>This is a simple drawscillator page.</p>
+        </div>
+    );
+}
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,8 @@
+export function Landing() {
+    return (
+        <div className="landing">
+        <h1>Welcome to the Landing Page</h1>
+        <p>This is a simple landing page.</p>
+        </div>
+    );
+}
